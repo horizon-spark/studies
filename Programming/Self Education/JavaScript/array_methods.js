6e8ad46c0aa6f4011c3ee91dccfafaec7f484a10
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-02-23 13:31:33 +0300

    Solved some array methods tasks

--- Programming/Self Education/JavaScript/array_methods.js
+++ Programming/Self Education/JavaScript/array_methods.js
@@ -63,3 +63,81 @@
 // alert( sorted ); 
 // alert( arr ); 
 
+// (6)
+// function Calculator() {
+//     this.methods = {
+//         "-": (a,b) => a - b,
+//         "+": (a,b) => a + b
+//     };
+//     this.calculate = function(str) {
+//         let arr = str.split(" ");
+//         let a = +arr[0];
+//         let op = arr[1];
+//         let b = +arr[2];
+//         if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//             return NaN;
+//         }
+//         return this.methods[op](a,b);
+//     }
+//     this.addMethod = function(name,func) {
+//         this.methods[name] = func;
+//     }
+// }
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result );
+
+// (7)
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map((obj) => obj.name);
+
+// alert( names ); // Вася, Петя, Маша
+
+// (8)
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map( user => ({
+//     fullName: `${user.name} ${user.surname}`,
+//     id: user.id
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+// (9)
+// function sortByAge(arr) {
+//     arr.sort((obj1, obj2) => obj1.age - obj2.age);
+// }
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
