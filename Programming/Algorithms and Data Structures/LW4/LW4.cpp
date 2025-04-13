9c4b21d3a90188f8f5a7ef0ee4c05b8f8632d51f
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-04-13 11:47:45 +0300

    Finished first version of lw4

--- Programming/Algorithms and Data Structures/LW4/LW4.cpp
+++ Programming/Algorithms and Data Structures/LW4/LW4.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <queue>
 #include <cmath>
 #include <ctime>
 
 using namespace std;
 
-int generatRandomInRange(pair<int,int> range)
+void printArr(vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << endl;
+    }
+    return;
+}
+
+int generatRandomInRange(pair<int,int> range) 
 {
     return rand() % (range.second - range.first + 1) + range.first;
 }
@@ -19,6 +27,14 @@ pair<int,int> getRangeByK(int k)
     return res;
 }
 
+int getDigitInPositionJ(int num, int j) 
+{
+    for (int i = 0; i < j; i++) {
+        num /= 10;
+    }
+    return num % 10;
+}
+
 vector<int> generateSequence(int n, int k)
 {
     vector<int> res;
@@ -29,12 +45,35 @@ vector<int> generateSequence(int n, int k)
     return res;
 }
 
+vector<int> LSDSort(vector<int> arr, int k)
+{
+    vector<int> res = arr;
+    vector<queue<int>> lsdArray(10);
+    for (int j = 0; j < k; j++) {
+        for (int i = 0; i < res.size(); i++) {
+            lsdArray[getDigitInPositionJ(res[i],j)].push(res[i]);
+        }
+        res.clear();
+        for (int i = 0; i < lsdArray.size(); i++) {
+            while(!lsdArray[i].empty()) {
+                res.push_back(lsdArray[i].front());
+                lsdArray[i].pop();
+            }
+        }
+        // cout << "After sorting by position " << j << ':' << endl;
+        // printArr(res);
+    }
+    return res;
+}
+
 int main()
 {
     srand(time(0));
     vector<int> vec = generateSequence(10, 4);
-    for (int i = 0; i < vec.size(); i++) {
-        cout << vec[i] << endl;
-    }
+    cout << "Generated sequence:" << endl;
+    printArr(vec);
+    vector<int> res = LSDSort(vec, 4);
+    cout << "After sort:" << endl;
+    printArr(res);
     return 0;
 }
