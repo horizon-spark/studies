ba831505e1de8425aa483d39e4b3d624f19f2350
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-05-07 18:38:00 +0300

    Working on lw6 (Switches doesn't count right)

--- Programming/Algorithms and Data Structures/LW6/LW6.cpp
+++ Programming/Algorithms and Data Structures/LW6/LW6.cpp
@@ -4,36 +4,59 @@
 
 using namespace std;
 
+int switches = 0;
+
+void printArray(vector<int>& arr)
+{
+    cout << "\t\t\t";
+    for (int i = 0; i < arr.size(); i++) {
+        if (i < arr.size() - 1) cout << arr[i] << ", ";
+        else cout << arr[i] << endl;
+    }
+}
+
 void Heap(int first, int last, vector<int>& arr)
 {
     int k, k21, k22;
     int buf;
     if (first <= last / 2) {
         k = first; k21 = 2 * first + 1; k22 = k21 + 1;
-        if ((k21 <= last) && (arr[first] < arr[k21]))
+        if ((k21 <= last) && (arr[first] < arr[k21])) {
             k = k21;
-        if ((k22 <= last) && (arr[k] < arr[k22]))
+        }
+        if ((k22 <= last) && (arr[k] < arr[k22])) {
             k = k22;
+        }
         if (k != first) {
             buf = arr[first]; arr[first] = arr[k]; arr[k] = buf;
+            switches++;
             Heap(k, last, arr);
         }
     }
 }
+
 void BuildSortTree(vector<int>& arr)
 {
     for (int i = arr.size() - 1; i >= 0; i--)
         Heap(i, arr.size() - 1, arr);
 }
+
 void HeapSort(vector<int>& arr)
 {
     int buf;
-    BuildSortTree(arr);
+    BuildSortTree(arr); 
+    cout << "After building sort tree: " << endl;
+    printArray(arr);
     for (int i = arr.size() - 1; i > 0; i--) {
         buf = arr[0]; arr[0] = arr[i]; arr[i] = buf;
+        cout << "Switches required: " << switches << endl;
+        switches = 0;
         Heap(0, i - 1, arr);
+        cout << "After iteration " << arr.size() - i << ":" << endl;
+        printArray(arr);
     }
 }
+
 void generateArray(vector<int>& arr) 
 {
     for (int i = 0; i < arr.size(); i++) {
@@ -41,13 +64,7 @@ void generateArray(vector<int>& arr)
     }
     return;
 }
-void printArray(vector<int>& arr)
-{
-    for (int i = 0; i < arr.size(); i++) {
-        if (i < arr.size() - 1) cout << arr[i] << ", ";
-        else cout << arr[i] << endl;
-    }
-}
+
 int main()
 {
     int n = 0;
