3138b704b633a3dbe941961036aa685471acb317
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-05-15 11:50:43 +0300

    Continued learning browser js

--- Programming/Self Education/JavaScript/DOM.js
+++ Programming/Self Education/JavaScript/DOM.js
@@ -1,11 +1,11 @@
 "use strict";
 // (1)
-// alert(document.body.children[0]);
+alert(document.body.children[0]);
 // // or
 // // document.body.firstElementChild
 // // or
 // // document.body.childNodes[1]
-// alert(document.body.children[1]);
+alert(document.body.children[1]);
 // // or
 // // document.body.lastElementChild
-// alert(document.body.children[1].childNodes[1]);
+alert(document.body.children[1].childNodes[1]);
