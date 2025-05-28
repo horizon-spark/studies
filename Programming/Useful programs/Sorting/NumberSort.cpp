6fd7494ebb820a22c58d00979c79c512c43ebc4c
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-05-28 17:20:47 +0300

    continued working on sorting program

--- Programming/Useful programs/Sorting/NumberSort.cpp
+++ Programming/Useful programs/Sorting/NumberSort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <fstream>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
@@ -28,11 +30,36 @@ void printArray(vector<float>& arr)
     return;
 }
 
+void printIntervals(vector<pair<float, float>>& intervals)
+{
+    for (int i = 0; i < intervals.size(); i++) {
+        cout << '[' << intervals[i].first << ';' << intervals[i].second << ')';
+        if (i < intervals.size() - 1) cout << ", ";
+        else cout << endl;
+    }
+}
+
 int main()
 {
     vector<float> arr = readArray("input.txt");
+    vector<pair<float, float>> intervals;
     sort(arr.begin(), arr.end());
+    int n = arr.size();
+    float range = arr.back() - arr.front();
+    int classesCount = floor(1.0 + 3.322 * log10(n));
+    float step = round(range / classesCount * 10.0) / 10.0;
+    pair<float, float> buf = make_pair(arr.front(), arr.front() + step);
+    intervals.push_back(buf);
+    for (int i = 1; i < classesCount; i++) {
+        buf.first = buf.second;
+        buf.second = buf.first + step;
+        intervals.push_back(buf);
+    }
     cout << "The sorted array: ";
     printArray(arr);
-    cout << "Size: " << arr.size() << endl;
+    cout << "Range: " << range << endl;
+    cout << "Number of classes: " << classesCount << endl;
+    cout << "Step: " << step << endl;
+    cout << "The calculated intervals: ";
+    printIntervals(intervals);
 }
